6c64039adbcfc2dcd8024b7e0d4fba321fc09454
Pavel196731 <pavzva200346@gmail.com>
2024-10-25 15:07:49 +0300

    Итоговый проект

--- app/src/main/java/com/example/my_application/MainActivity.kt
+++ app/src/main/java/com/example/my_application/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.my_application
 
+import android.graphics.fonts.FontStyle
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -17,6 +18,7 @@ import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -27,14 +29,16 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MainScreen() // Главный экран
+            MainScreen()
         }
     }
 }
@@ -42,51 +46,50 @@ class MainActivity : ComponentActivity() {
 @Preview(showSystemUi = true)
 @Composable
 fun MainScreen() {
-    var squares by rememberSaveable { mutableStateOf(listOf<Int>()) } // Сохранение списка квадратиков
-    val gridState = rememberLazyGridState() // Запоминаем состояние для управления прокруткой
-
-    // Получаем количество колонок через отдельную функцию
+    var kvadrat by rememberSaveable { mutableStateOf(listOf<Int>()) } // список квадратиков
+    val state = rememberLazyGridState() // состояния для скролла
     val columns = getGridColumns()
 
-    // Основной контейнер
     Box(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
     ) {
-        // Отображаем сетку квадратов с отступом снизу
-        GridSquares(squares = squares, columns = columns, paddingBottom = 80.dp, state = gridState)
-        // Кнопка для добавления квадратов
+        GridKvadrat(kvadrat = kvadrat, columns = columns, paddingBottom = 80.dp, state = state)
         AddSquareButton(
             onClick = {
-                squares = squares + (squares.size + 1) // Добавляем новый квадрат
+                kvadrat = kvadrat + (kvadrat.size + 1)
             }
         )
+        if (kvadrat.isNotEmpty() && kvadrat.size>columns) {
+            LaunchedEffect(kvadrat.size) {
+                state.scrollToItem(kvadrat.size - (kvadrat.size % columns))
+            }
+        }
     }
 }
 
-// Вынесенная функция для определения количества колонок в зависимости от ориентации
 @Composable
 fun getGridColumns(): Int {
-    val configuration = LocalConfiguration.current
-    return if (configuration.orientation == android.content.res.Configuration.ORIENTATION_LANDSCAPE) {
-        4 // Ландшафтная ориентация
+    val location = LocalConfiguration.current
+    return if (location.orientation == android.content.res.Configuration.ORIENTATION_LANDSCAPE) {
+        4
     } else {
-        3 // Портретная ориентация
+        3
     }
 }
 
 @Composable
-fun GridSquares(squares: List<Int>, columns: Int, paddingBottom: Dp, state: LazyGridState) {
+fun GridKvadrat(kvadrat: List<Int>, columns: Int, paddingBottom: Dp, state: LazyGridState) {
     LazyVerticalGrid(
-        state = state, // Передаем состояние для управления скроллом
-        columns = GridCells.Fixed(columns), // Устанавливаем количество колонок
+        state = state, // скролл
+        columns = GridCells.Fixed(columns), // количество колонок
         modifier = Modifier.run {
             fillMaxSize()
                 .padding(bottom = paddingBottom)
-        }, // Добавляем отступ снизу
+        },
         contentPadding = PaddingValues(16.dp)
     ) {
-        // Отображаем каждый квадрат
-        items(squares.size) { index ->
+        items(kvadrat.size) { index ->
             GridItem(index = index)
         }
     }
@@ -94,19 +97,21 @@ fun GridSquares(squares: List<Int>, columns: Int, paddingBottom: Dp, state: Lazy
 
 @Composable
 fun GridItem(index: Int) {
-    // Условие для смены цвета: чётные (красные), нечётные (синие)
     val color = if (index % 2 == 0) Color.Red else Color.Blue
-
 
     Box(
         modifier = Modifier
-            .padding(4.dp) // Отступ между квадратами
-            .aspectRatio(1f) // Пропорция 1:1 для квадратов
-            .background(color), // Устанавливаем цвет в зависимости от индекса
+            .padding(4.dp)
+            .aspectRatio(1f)
+            .background(color),
         contentAlignment = Alignment.Center
     ) {
-        // Текст внутри квадрата
-        Text(text = "${index + 1}", color = Color.White)
+        Text(
+            text = "${index + 1}",
+            fontSize = 25.sp,
+           // fontStyle = FontStyle.Italic,
+            fontWeight = FontWeight.Medium,
+            color = Color.White)
     }
 }
 
@@ -119,7 +124,7 @@ fun AddSquareButton(onClick: () -> Unit) {
             onClick = onClick,
             modifier = Modifier
                 .align(Alignment.BottomEnd)
-                .padding(16.dp) // Кнопка будет внизу справа
+                .padding(16.dp)
         ) {
             Text("Click Me")
         }
